1266196a651c87a85d99728dbb9b1c2c80dd8705
EdgarAlonsoPerez <ingperezedgar@gmail.com>
2024-07-09 23:06:21 -0600

    feat(tailwind): :wrench: initialize the confiiguration files and index file.

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    theme: {
+        screens: {
+            sm: '640px',
+            md: '768px',
+            lg: '976px',
+            xl: '1440px'
+        },
+        extend: {
+            fontFamily: {
+                sans: ['Josefin Sans', 'sans-serif']
+            },
+            alata: ['Alata']
+        }
+    },
+    plugins: []
+}
